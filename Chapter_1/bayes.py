28ed64686fd4a07b4ffc437f33b100e164eeb514
Matthew Beamer <mattbmw@gmail.com>
2021-11-17 15:48:37 -0500

    Update name of Bayes Challenge
    Reflect that this is for first challenge

--- Chapter_1/bayes.py
+++ Chapter_1/bayes.py
@@ -161,6 +161,7 @@ def main():
     print("-" * 65)
     print("\nInitial Target (P) Probabilities:")
     print("P1 = {:.3f}, P2 = {:.3f}, P3 = {:.3f}".format(app.p1, app.p2, app.p3))
+    print("Max Prob = {:.3f}".format(max(app.p1, app.p2, app.p3)))
     search_num = 1
 
     while True:
